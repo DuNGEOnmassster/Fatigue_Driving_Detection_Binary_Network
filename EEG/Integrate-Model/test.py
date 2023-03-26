cd31ce2f6e122ea1df0e747ee385aa180a4e1e22
NormanZ <1275133384@qq.com>
2023-03-26 23:47:53 +0800

    update inference

--- EEG/Integrate-Model/test.py
+++ EEG/Integrate-Model/test.py
@@ -6,7 +6,8 @@ from config import config as cfg
 from train import DataLoad, load_model
 
 
-def init_test(test_model):
+## test
+def get_test_data(test_model):
     all_data = DataLoad()
     test_data = torch.from_numpy(all_data.test_data).float().to(cfg.device)
     # print(test_data.shape)
@@ -23,7 +24,7 @@ def init_test(test_model):
 
 
 def test(test_model):
-    model, test_data = init_test(test_model)
+    model, test_data = get_test_data(test_model)
     model.eval()
     labels_test = test_data[:, 0].long()
     output = model(test_data[:, 1:])
@@ -35,25 +36,48 @@ def test(test_model):
     print(f"Test Acc = {correct_test} / {labels_test.shape[0]}, Acc rate = {correct_test/labels_test.shape[0]}")
 
 
-def get_inferdata(test_model):
-    _, test_data = init_test(test_model)
+## inference
+def get_infer_data(test_model):
+    all_data = DataLoad()
+    test_data = torch.from_numpy(all_data.test_data).float().to(cfg.device)
+    # print(test_data.shape)
+    model = SleepModel(5, is_training=True)
+
+    model = model.to(cfg.device)
+    if cfg.cuda:
+        cudnn.benchmark = True
+
+    if cfg.resume:
+        load_model(model, test_model)
+
     test_data = test_data[:, 1:]
 
-    return test_data
+    return model, test_data
 
 
 def pred_count(pred_test):
-    
+    count = {i: 0 for i in range(5)}
+    for i, label in enumerate(pred_test):
+        # print(i, label.item())
+        count[label.item()] += 1
+    return count
+
+
+def trans_fatigue_level(count):
+    max_label = max(count, key=count.get)
+
 
 
 def inference(test_model):
-    model, test_data = init_test(test_model)
-    infer_data = get_inferdata(test_model)
+    model, infer_data = get_infer_data(test_model)
     model.eval()
     output = model(infer_data)
     # print(output.shape)
     pred_test = output.data.max(1, keepdim=True)[1]
-    print(pred_test)
+    # print(pred_test)
+    count = pred_count(pred_test)
+    # print(count)
+    trans_fatigue_level(count)
 
 
 if __name__ == "__main__":
