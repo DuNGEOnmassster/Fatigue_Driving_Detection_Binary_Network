44864bc28cae6ae18da243803827618546574636
finalswordsss <109805874+finalswordsss@users.noreply.github.com>
2023-03-28 16:31:45 +0800

    Add files via upload

---
+++ EEG/util/bo_classify.py
@@ -0,0 +1,36 @@
+from scipy.signal import butter, filtfilt
+import numpy as np
+import scipy.io as scio
+def butter_bandpass(lowcut, highcut, fs, order):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = filtfilt(b, a, data)
+    return y
+dataFile = 'dataOut4.mat'
+data = scio.loadmat(dataFile)
+#print(data.keys())
+#print(data['temp'][1:,:])
+data1 = np.sin(data['temp'])
+data2 = np.sin(data['temp'])
+data3 = np.sin(data['temp'])
+data4 = np.sin(data['temp'])
+
+fs = 1000
+
+
+a = butter_bandpass_filter(data1, 8, 13, fs, 5)#
+a = np.mean(np.abs(a))
+b= butter_bandpass_filter(data2, 14, 25, fs, 5)
+b = np.mean(np.abs(b))
+t= butter_bandpass_filter(data3, 4, 7, fs, 1)
+t = np.mean(np.abs(t))
+d= butter_bandpass_filter(data4, 0.5, 4, fs, 1)
+d = np.mean(np.abs(d))
+arr = np.array([a, b, t, d])
+print(arr)
